1cf7c4df604b9912da693735f77176a19e236135
Anastasija042 <anastasija_042@mail.ru>
2023-06-28 14:31:32 +0200

    corrected

---
+++ script.js
@@ -0,0 +1,56 @@
+// Задание 1
+let a = 10;
+alert(a);
+// Задание 2
+a = 20;
+alert(a);
+year = 2007
+print(year)
+// Задание 3
+let creator = "Брендан Эйх";
+console.log(creator);
+// Задание 4
+let a = 10;
+let b = 2;
+alert(Сумма: ${a + b});
+alert(Разность: ${a - b});
+alert(Произведение: ${a * b});
+alert(Частное: ${a / b});
+// Задание 5
+result = 2 ** 5;
+alert(result);
+// Задание 6
+let a = 9;
+let b = 2;
+let remainder = a % b;
+alert(remainder);
+// Задание 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+// Задание 8
+let age = prompt("Сколько вам лет?");
+alert("Вам " + age + " лет.");
+// Задание 9.0
+let user = {
+    name: 'Anna',
+    age: 30,
+    isAdmin: true,
+    };
+// Задание 9.1
+user["city of residence"] = "Калининград";
+// Задание 9.2
+user.age = 12;
+// Задание 9.3
+delete ["city of residence"]
+// Задание 9.4
+let info = prompt("Какую информацию хотите узнать о пользователе?");
+alert(user[info]);
+// Задание 10
+let name = prompt("Введите Ваше имя");
+alert("Привет, " + "name" + "!");
