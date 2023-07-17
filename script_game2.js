a1203805f514857c83ce9f6edaf589abe6e64259
Anastasija042 <anastasija_042@mail.ru>
2023-07-17 23:46:11 +0200

    work_and_trouble

--- script_game2.js
+++ script_game2.js
@@ -9,7 +9,7 @@ function wordsGame() {
     let lastWord = prompt("Чему равнялся последний элемент массива?");
 
     if (firstWord === words[0] && lastWord === words[words.length - 1]) {
-        alert("Поздравляем! Вы угадали оба элемента!");
+        alert("Поздравляем!Вы угадали оба элемента!");
     } 
     else if (firstWord === words[0] || lastWord === words[words.length - 1]) {
         alert("Вы были близки к победе!");
